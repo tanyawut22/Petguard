39e18a7d30bb1bbf7851d835d800a938df97979d
unknown <jpt70300001@gmail.com>
2021-06-16 02:50:48 +0700

    fix

--- js/login.js
+++ js/login.js
@@ -8,6 +8,8 @@ const email = document.getElementById("email"),
     console.log(password.value);
     firebase.auth().signInWithEmailAndPassword(email.value, password.value).then((auth) =>{
         console.log("Login success");
+        alert("Login success");
+        window.location = "index.html";
     })
     .catch(function(error) {
         // Handle Errors here.
